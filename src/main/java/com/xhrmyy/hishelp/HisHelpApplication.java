256842e25436e3fdb856d87063d3797f14bd2d12
hsm <904837992@qq.com>
2018-10-10 22:16:50 +0800

    项目创建

---
+++ src/main/java/com/xhrmyy/hishelp/HisHelpApplication.java
@@ -0,0 +1,12 @@
+package com.xhrmyy.hishelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisHelpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HisHelpApplication.class, args);
+    }
+}
