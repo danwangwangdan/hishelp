7d0f3e23034257a83efbbda55ecd9dfad030ffa1
hsm <hsm19950709@gmail.com>
2019-03-14 16:46:56 +0800

    每周推送消息

--- src/main/java/com/xhrmyy/hishelp/HisHelpApplication.java
+++ src/main/java/com/xhrmyy/hishelp/HisHelpApplication.java
@@ -2,11 +2,13 @@ package com.xhrmyy.hishelp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by huangshiming on 2017/12/27.
  */
 @SpringBootApplication
+@EnableScheduling
 public class HisHelpApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/xhrmyy/hishelp/task/SendNotificationTask.java
+++ src/main/java/com/xhrmyy/hishelp/task/SendNotificationTask.java
@@ -7,6 +7,7 @@ import com.xhrmyy.hishelp.util.WeChatUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -19,21 +20,23 @@ import java.util.Map;
  * @Date 2019-3-14
  */
 @Component
+@EnableScheduling
 public class SendNotificationTask {
     private static final Logger log = LoggerFactory.getLogger(SendNotificationTask.class);
 
     @Autowired
     private TroubleService troubleService;
 
-    @Scheduled(cron = "0 0 12 ? * THU")    // 每周一9点发送
+    @Scheduled(cron = "0 0 9 ? * MON")    // 每周一9点发送
     public void test() {
         try {
             //要执行的任务逻辑写在这里
+            log.info("开始定时发送消息");
             Map<String, TemplateData> dataMap = new HashMap<>();
             dataMap.put("keyword1", new TemplateData("收集模版消息ID提醒"));
             dataMap.put("keyword2", new TemplateData("每周任务，请多次点击对应按钮来获取推送消息的机会。"));
             TemplateMessage templateMessage = new TemplateMessage();
-            templateMessage.setTemplate_id(WeChatUtil.TEMPLE_MESSAGE_SUBMITTED);
+            templateMessage.setTemplate_id(WeChatUtil.TEMPLE_MESSAGE_NOTE);
             templateMessage.setPage(WeChatUtil.GO_PAGE_ME);
             templateMessage.setTouser(WeChatUtil.ADMIN_OPEN_ID.get("黄士明"));
             templateMessage.setData(dataMap);
