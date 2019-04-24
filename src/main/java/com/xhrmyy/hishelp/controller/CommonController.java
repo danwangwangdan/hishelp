0620684b9fb990e2cc1f701fedf6bd4697324edb
hsm <hsm19950709@gmail.com>
2019-04-24 14:08:05 +0800

    队列转接接口

--- src/main/java/com/xhrmyy/hishelp/controller/CommonController.java
+++ src/main/java/com/xhrmyy/hishelp/controller/CommonController.java
@@ -1,9 +1,11 @@
 package com.xhrmyy.hishelp.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.xhrmyy.hishelp.common.BaseResult;
 import com.xhrmyy.hishelp.entity.Suggestion;
 import com.xhrmyy.hishelp.entity.Trouble;
 import com.xhrmyy.hishelp.service.CommonService;
+import com.xhrmyy.hishelp.util.HttpUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +29,24 @@ public class CommonController {
     private static final Logger log = LoggerFactory.getLogger(CommonController.class);
     @Autowired
     private CommonService commonService;
+
+    @RequestMapping("/queue")
+    public BaseResult getQueueInfo(@RequestParam String office, @RequestParam String room, @RequestParam String ca) {
+
+        String authUrl = "http://24z56z0190.zicp.vip/histool/queue/list?office=OFFICE&room=ROOM&ca=CA";
+        BaseResult baseResult = null;
+        authUrl = authUrl.replace("OFFICE", office);
+        authUrl = authUrl.replace("ROOM", room);
+        authUrl = authUrl.replace("CA", ca);
+        String resultJson = HttpUtil.sendGet(authUrl);
+        try {
+            baseResult = JSON.parseObject(resultJson, BaseResult.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        log.info("获取微信信息：" + baseResult.toString());
+        return baseResult;
+    }
 
     @RequestMapping("/dystatus")
     public BaseResult getDyStatus() {
